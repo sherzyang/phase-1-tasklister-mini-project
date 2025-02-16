8dfb3a1d7c697430a5c6ba3f9b8f7b5e1a5ec9e4
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-02-15 16:37:51 -0800

    Update.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    let getForm = document.getElementById("new-task-description")
+    handleTasks(getForm.value)
+    form.reset()
+  })
 });
+
+function handleTasks(toDo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  p.textContent = `${toDo}  `;
+  btn.textContent = "💕"
+  p.appendChild(btn)
+  document.querySelector("#tasks").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
