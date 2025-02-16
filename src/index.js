334a853ac3527aed9cc318b96b383164a570c6b4
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-02-16 12:45:48 -0800

    Update.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let form = document.querySelector("form")
+  
+  form.
+
   form.addEventListener('submit', (e) =>{
     e.preventDefault()
     let getForm = document.getElementById("new-task-description")
     handleTasks(getForm.value)
     form.reset()
   })
+
   function handleTasks(toDo) {
     let p = document.createElement('p')
     let btn = document.createElement('button')
@@ -20,5 +24,20 @@ document.addEventListener("DOMContentLoaded", () => {
   function handleDelete(e){
     e.target.parentNode.remove()
   }
+
+  let select = document.createElement('select')
+  form.appendChild(select)
+  let optionOne = document.createElement('option')
+  optionOne.innerText = "Easy"
+  select.appendChild(optionOne)
+  let optionTwo = document.createElement('option')
+  optionTwo.innerText = "Medium"
+  select.appendChild(optionTwo)
+  let optionThree = document.createElement('option')
+  optionThree.innerText = "Challenging"
+  select.appendChild(optionThree)
+  
 });
+
+
 
